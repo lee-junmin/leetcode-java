9e82b200fce93ebf6c3071cea238ad6cd5cf00c3
lee-junmin <junminlee3@gmail.com>
2022-04-06 08:36:07 +1000

    add sum of two integers

---
+++ src/SumOfTwoIntegers/Solution.java
@@ -0,0 +1,48 @@
+
+/*
+
+SUMMARY
+keep on adding the sum with no carry (a XOR b), and value with only carry (a AND b) << 1.
+reassign each to a and b and loop until b becomes 0
+
+MAIN IDEA
+for each position the sum can either be
+case 1. 0,  when a=0 b=0
+case 2. 1,  when a=1 b=0 or a=0 b=1
+case 3. 10, when a=1 b=1
+if you just think about the last position of the result,
+this can be a XOR result of each position for a and b
+
+but we need to think about the carry value for case 3
+there is a carry value only when a=1 AND b=1
+we can add (a AND b and bit shift left once) to the result to reflect the carry value
+
+the adding of the carry value should go on until there is no carry value to add
+
+to make the code simpler for each loop we will
+re-assign a to a XOR b
+re-assign b to the carry value to be added
+and keep on looping until b becomes 0
+
+ */
+
+package SumOfTwoIntegers;
+
+public class Solution {
+  public int getSum(int a, int b) {
+    while (b != 0){
+      int temp = a;
+      a = a ^ b;
+      b = (temp & b) << 1;
+    }
+    return a;
+  }
+  
+  
+  public static void main (String[] args){
+    Solution s = new Solution();
+    int answer = s.getSum(3,4);
+    System.out.println(answer);
+    
+  }
+}
