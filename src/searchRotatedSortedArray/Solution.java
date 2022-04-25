8b775e56a4fbe50c985b7ee5a955442b27597536
lee-junmin <103478446+jlee-sunday@users.noreply.github.com>
2022-04-25 10:25:01 +1000

    add solution

--- src/searchRotatedSortedArray/Solution.java
+++ src/searchRotatedSortedArray/Solution.java
@@ -1,5 +1,8 @@
 package searchRotatedSortedArray;
 
+// 4 5 6 7 0 1 2 3
+// l     m       h
+
 class Solution {
     public int search(int[] nums, int target) {
 
@@ -11,16 +14,17 @@ class Solution {
             if (nums[mid] == target) return mid;
 
             if (nums[lo] <= nums[mid]){
-                if(target < nums[lo] ||  target > nums[mid]){
-                    lo = mid + 1;
-                } else {
+                if (target >= nums[lo] && target < nums[mid]){
                     hi = mid - 1;
+                } else {
+                    lo = mid + 1;
                 }
+
             } else {
-                if (target < nums[mid] || target > nums[hi]){
-                    hi = mid - 1;
-                } else {
+                if  (target > nums[mid] && target <= nums[hi]){
                     lo = mid + 1;
+                } else {
+                    hi = mid - 1;
                 }
             }
         }
