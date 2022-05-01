58052f1a91030082b82db05b75948ce607803229
lee-junmin <junminlee3@gmail.com>
2022-05-01 16:05:42 +1000

    add solution

--- src/Playground.java
+++ src/Playground.java
@@ -3,10 +3,10 @@ import java.util.*;
 public class Playground {
   
   public static void main(String[] args){
- 
-  int len = 4;
-   System.out.println(++len);
-   
+    HashMap<Integer, ArrayList<Integer>> adjMap = new HashMap<>();
+    adjMap.put(1, new ArrayList<>());
+    System.out.print(adjMap.get(1));
   }
+ 
   
 }
