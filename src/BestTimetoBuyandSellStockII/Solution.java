0bd48ba7fd8eb1f51c3b31d3c44e096e34caf16d
LazyPanda397 <gauravr397@gmail.com>
2022-06-17 10:03:07 +0530

    122. Best Time to Buy and Sell Stock II

---
+++ src/BestTimetoBuyandSellStockII/Solution.java
@@ -0,0 +1,25 @@
+package BestTimetoBuyandSellStockII;
+
+class Solution {
+    /*
+     * 122. Best Time to Buy and Sell Stock II
+     * 
+     * Example 1:
+     * Input: prices = [7,1,5,3,6,4]
+     * Output: 7
+     * Explanation: Buy on day 2 (price = 1) and sell on day 3 (price = 5), profit =
+     * 5-1 = 4.
+     * Then buy on day 4 (price = 3) and sell on day 5 (price = 6), profit = 6-3 =
+     * 3.
+     * Total profit is 4 + 3 = 7.
+     */
+
+    public int maxProfit(int[] prices) {
+        int maxprofit = 0;
+        for (int i = 1; i < prices.length; i++) {
+            if (prices[i] > prices[i - 1])
+                maxprofit += prices[i] - prices[i - 1];
+        }
+        return maxprofit;
+    }
+}
