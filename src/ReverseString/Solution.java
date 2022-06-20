1dcce83b8ea205d4b6a47a6104313fe837f553e1
lee-junmin <junminlee3@gmail.com>
2022-06-20 10:16:58 +1000

    add solution

---
+++ src/ReverseString/Solution.java
@@ -0,0 +1,13 @@
+package ReverseString;
+
+class Solution {
+    public void reverseString(char[] s) {
+        int len = s.length;
+        for (int headIndex=0; headIndex<len/2; headIndex++){
+            int tailIndex= len - 1 - headIndex;
+            char temp = s[headIndex];
+            s[headIndex] = s[tailIndex];
+            s[tailIndex] = temp;
+        }
+    }
+}
